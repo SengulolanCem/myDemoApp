fb89449e0973033887c8346d21d6870008b46323
SengulolanCem <c.sengul@etu.edu.tr>
2017-09-25 10:28:49 -0700

    First commit.

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,29 @@
+package com.mycompany.app;
+
+import java.util.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+       public static boolean search(ArrayList<Integer> array, int e) {
+        System.out.println("inside search");
+        if (array == null) return false;
+  
+        for (int elt : array) {
+          if (elt == e) return true;
+        }
+        return false;
+      }
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public int sum(int a,int b,int c)
+    {
+	return a+b+c;
+    }
+}
+	
